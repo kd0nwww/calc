382e090b863e57227805d3394168043d48c7033a
Kirill Donetskov <kirilldonetskov@gmail.com>
2023-11-27 02:07:49 +0600

    added menu

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
+
+print("What we gonna do with these numbers?")
+print("1 - Addition")
+print("2 - Subtraction")
+print("3 - Division")
+print("4 - Multiplication")
+choice = int(input())
+
+input()
